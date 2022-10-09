2909977cbde514b2cfdd4708133fe3d0066d3589
johnchakkour <john.chakkour@gmail.com>
2022-10-09 19:56:59 -0400

    Finished all tasks.

--- src/main/java/DrivableTrader.java
+++ src/main/java/DrivableTrader.java
@@ -1,4 +1,4 @@
-/* TODO: Create a subclass of Trader named DrivableTrader
+/*
  * This class should be identical to Trader, except that it takes
  * only Drivable objects in its inventory, wishlist, etc.
  *
@@ -11,3 +11,37 @@
  */
 
 import java.util.List;
+
+class DrivableTrader extends Trader<Drivable> {
+    /**
+     * Construct a DomesticatableTrader, giving them the
+     * given inventory, wishlist, and money.
+     *
+     * @param inventory Objects in this Trader's inventory
+     * @param wishlist  Objects in this Trader's wishlist
+     * @param money     The Trader's money
+     */
+    public DrivableTrader(List<Drivable> inventory,
+                          List<Drivable> wishlist,
+                          int money) {
+        super(inventory, wishlist, money);
+    }
+
+    /**
+     * Construct a DomesticatableTrader, giving them the
+     * given money and an empty wishlist and inventory.
+     *
+     * @param money     The Trader's money
+     */
+    public DrivableTrader(int money) { super(money); }
+
+    @Override
+    public int getSellingPrice(Drivable item) {
+        if (item instanceof Tradable) {
+            return ((Tradable) item).getPrice() + item.getMaxSpeed();
+        } else {
+            return Tradable.MISSING_PRICE;
+        }
+
+    }
+}
